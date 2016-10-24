0518f7e1ab364f57d3c749f63360acd5982e3e94
steve0hh <stevettan@gmail.com>
2016-10-24 11:56:46 +0800

    Clean up authentication scope for routers

--- web/router.ex
+++ web/router.ex
@@ -14,33 +14,35 @@ defmodule VirtualJudge.Router do
     plug :accepts, ["json"]
   end
 
+  # unauthenticated scope
   scope "/", VirtualJudge do
-    pipe_through :browser # Use the default browser stack
-
+    pipe_through [:browser] # Use the default browser stack
     get "/", PageController, :index
+    resources "/sign_up", RegistrationController, only: [:new, :create]
+    resources "/sign_in", SessionController, only: [:new, :create, :delete]
+    get "/sign_up/:id/:invitation_token", RegistrationController, :edit
+    put "/sign_up/:id/:invitation_token", RegistrationController, :update
+  end
+
+  # normal user authenticated scope
+  scope "/", VirtualJudge do
+    pipe_through [:browser, :authenticate_user] # Use the default browser stack
     resources "/problem", ProblemController, only: [:index, :show] do
       resources "/answer", AnswerController, only: [:create]
     end
-
     resources "/answer", AnswerController, only: [:index, :show]
-
-    resources "/sign_up", RegistrationController, only: [:new, :create]
-    resources "/sign_in", SessionController, only: [:new, :create, :delete]
-
-    get "/sign_up/:id/:invitation_token", RegistrationController, :edit
-    put "/sign_up/:id/:invitation_token", RegistrationController, :update
-
-    scope "/admin", Admin, as: :admin do
-      get "/invite", InvitationController, :new
-      post "/invite", InvitationController, :create
-      get "/users", UserController, :index
-      get "/export", UserController, :export
-      resources "/contest", ContestController
-    end
-
     resources "/contest", ContestController
 
+  end
 
+  # admin scope
+  scope "/admin", VirtualJudge.Admin, as: :admin do
+    pipe_through [:browser] # Use the default browser stack
+    get "/invite", InvitationController, :new
+    post "/invite", InvitationController, :create
+    get "/users", UserController, :index
+    get "/export", UserController, :export
+    resources "/contest", ContestController
   end
 
   # Other scopes may use custom stacks.
