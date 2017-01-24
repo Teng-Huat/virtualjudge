8af815b041c7224879b3bda995a904e8d7e01391
steve0hh <stevettan@gmail.com>
2017-01-24 13:31:34 +0800

    Fix #15, add stylesheet prepending to iframe

--- web/views/shared_view.ex
+++ web/views/shared_view.ex
@@ -1,3 +1,28 @@
 defmodule VirtualJudge.SharedView do
   use VirtualJudge.Web, :view
+  def link_css("http://acm.timus.ru" <> _path) do
+    do_link_css("http://acm.timus.ru/style50.css")
+  end
+
+  def link_css("http://codeforces.com/" <> _path) do
+    do_link_css("http://st.codeforces.com/s/12336/css/problem-statement.css")
+  end
+
+  def link_css("http://poj.org/" <> _path) do
+    do_link_css("http://poj.org/poj.css")
+  end
+
+  def link_css("http://acm.hust.edu.cn/" <> _path) do
+    do_link_css("http://acm.hust.edu.cn/css/themes/bootstrap.css")
+  end
+
+  def link_css("http://www.lydsy.com/JudgeOnline/" <> _path) do
+    do_link_css("http://www.lydsy.com/JudgeOnline/include/hoj.css")
+  end
+
+  def link_css(_source_url), do: ""
+
+  def do_link_css(source) do
+    "<link rel=\"stylesheet\" href=\""<> source <>"\" type=\"text/css\" charset=\"utf-8\">"
+  end
 end
